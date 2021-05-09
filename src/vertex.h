f84ccb40f5df7ee402d6fb74ae5ebb8cadfcfb0a
Haydeni0 <hayden.dorahy@gmail.com>
2021-05-09 13:28:14 +0100

    Define Position and Colour outside of vertex so they can be reused

--- src/vertex.h
+++ src/vertex.h
@@ -6,22 +6,27 @@ that contains position (in 2D) and colour
 #pragma once
 #include <GL/glew.h>
 
+struct Position
+{
+    float x;
+    float y;
+};
+
+struct Colour
+{
+    GLubyte r;
+    GLubyte g;
+    GLubyte b;
+    GLubyte a;
+};
+
 struct Vertex
 {
-    // Structs inside of a struct
-    struct Position
-    {
-        float x;
-        float y;
-    } position;
+    // Structs inside of a struct (composition)
+    // Position of the vertex
+    Position position;
     // Use just one byte to store each colour (0-255)
     // Red, Green, Blue and alpha
-    struct Colour
-    {
-        GLubyte r;
-        GLubyte g;
-        GLubyte b;
-        GLubyte a;
-    } colour;
+    Colour colour;
     // The Vertex struct needs to have a multiple of 4 bytes for alignment
 };
